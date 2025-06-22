0779cec11523fd8bb4426156777883fa5c3bfcbd
yukidaruma <yukidaruma@users.noreply.github.com>
2025-06-22 17:29:59 +0900

    chore: Fix broken dev server after running `next build`

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  // https://github.com/vercel/next.js/issues/61228#issuecomment-2002035619
+  distDir: process.env.NODE_ENV === "development" ? ".next/dev" : ".next/build",
 };
 
 export default nextConfig;
